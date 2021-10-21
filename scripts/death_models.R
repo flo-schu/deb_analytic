dcc47de9f1494edc0c34d51454947e2f42757cbb
flo-schu <fluncki@protonmail.com>
2021-10-21 17:46:45 +0200

    improve readability

--- scripts/death_models.R
+++ scripts/death_models.R
@@ -3,7 +3,7 @@
 max_time <- 150 # let's say days
 timestep <- 0.1
 samples <- 5000
-time <- seq(0, max_time / timestep)
+time <- seq(0, max_time / timestep)  # adapt observation frequency
 death_models <- list()
 
 # constant death rate
@@ -13,19 +13,21 @@ death_models[["constant"]] <- rep(0.02, length(time)) * timestep
 death_models[["linear"]] <- seq(0, .1, length.out = length(time)) * timestep
 
 
-observed_deaths <- matrix(nrow = samples, ncol = length(death_models))
+survival_time <- matrix(nrow = samples, ncol = length(death_models))
 for (m in 1:length(death_models)) {
     for (i in 1:samples) {
         survival <- rbinom(max(time), 1, 1 - death_models[[m]])
-        observed_deaths[i, m] <- which(survival == 0)[1] * timestep
-        observed_deaths <- ifelse(is.na(observed_deaths), Inf, observed_deaths)
-        colnames(observed_deaths) <- names(death_models)
+        survival_time[i, m] <- which(survival == 0)[1]
+        survival_time <- ifelse(is.na(survival_time), Inf, survival_time)
+        colnames(survival_time) <- names(death_models)
+
     }
 }
 
+survival_time <- survival_time * timestep  # convert time back to days
 
 par(mfrow = c(2, 2))
 plot(time, death_models[["constant"]])
-hist(observed_deaths[, 1], xlim = c(0, max_time))
+hist(survival_time[, 1], xlim = c(0, max_time))
 plot(time, death_models[["linear"]])
-hist(observed_deaths[, 2], xlim = c(0, max_time))
+hist(survival_time[, 2], xlim = c(0, max_time))
